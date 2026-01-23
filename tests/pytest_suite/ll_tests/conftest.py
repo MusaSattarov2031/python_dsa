60f213fe5d0de48aaa6e6f3517f9ad80a8072095
MusaSattarov2031 <musasattarov855@gmail.com>
2026-01-23 22:06:44 +0300

    feat: doctrins for test fixtures for linked list

--- tests/pytest_suite/ll_tests/conftest.py
+++ tests/pytest_suite/ll_tests/conftest.py
@@ -3,6 +3,7 @@ from src.linked_list import LinkedList
 
 @pytest.fixture(params=["empty", "single", "multiple"])
 def ll_all_cases(request):
+    """Tests for empty, single noded and multiple noded linked lists"""
     ll = LinkedList()
     if request.param == "single":
         ll.append(10)
@@ -12,16 +13,19 @@ def ll_all_cases(request):
 
 @pytest.fixture
 def empty_ll():
+    """Tests an empty list"""
     return LinkedList()
 
 @pytest.fixture
 def single_ll():
+    """Tests for single noded linked list"""
     ll = LinkedList()
     ll.append(10)
     return ll
 
 @pytest.fixture
 def multiple_ll():
+    """Tests for multiple noded linked list"""
     ll = LinkedList()
     ll.append(10, 20, 30)
     return ll
