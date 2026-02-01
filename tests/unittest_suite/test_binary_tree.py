0209b1088bbd90375de9b8240ade275694844474
MusaSattarov2031 <musasattarov855@gmail.com>
2026-02-01 16:31:57 +0300

    test: complete unittest test suite for binary tree

--- tests/unittest_suite/test_binary_tree.py
+++ tests/unittest_suite/test_binary_tree.py
@@ -1,5 +1,5 @@
 import unittest
-import sys
+import random
 from src.binary_tree import BinaryTree
 
 
@@ -51,4 +51,45 @@ class TestBinaryTree(unittest.TestCase):
         self.bt.delete_node(5)
         self.assertEqual(str(self.bt), "0 1 2 3 4 6 7 8 9")
 
+    def test_search(self):
+        self.bt.add_node(5)
+        for i in range(100):
+            self.bt.add_node(i)
 
+        self.assertTrue(self.bt.search(random.randint(0, 99)))
+        self.assertFalse(self.bt.search((random.randint(0, 99))*(-1)))
+    
+    def test_get_height(self):
+        self.assertEqual(self.bt.get_height(), -1)
+        self.bt.add_node(5)
+        self.assertEqual(self.bt.get_height(), 0)
+
+        for i in range(10):
+            self.bt.add_node(i)
+
+        self.assertEqual(self.bt.get_height(), 5)
+
+    def test_as_list(self):
+        self.bt.add_node(5)
+        for i in range(10):
+            self.bt.add_node(i)
+
+        self.assertEqual(str(self.bt.as_list()), "[5, 0, 6, 1, 7, 2, 8, 3, 9, 4]")
+
+    def test_clear(self):
+        self.bt.add_node(50)
+        for i in range(100):
+            self.bt.add_node(i)
+        self.bt.clear()
+        self.assertEqual(len(self.bt), 0)
+
+
+    def test_min_max(self):
+        self.bt.add_node(50)
+        for i in range(100):
+            self.bt.add_node(i)
+        self.assertEqual(self.bt.find_max(), 99)
+        self.assertEqual(self.bt.find_min(), 0)
+        self.bt.clear()
+        with self.assertRaises(ValueError):
+            MAX = self.bt.find_max()
